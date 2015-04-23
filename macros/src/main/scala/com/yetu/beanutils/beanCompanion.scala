3b13f70132aecd40762f2a05588208841f56c7a4
Mario Camou <mario.camou@yetu.com>
2015-04-23 14:43:24 +0200

    Cleanup

--- macros/src/main/scala/com/yetu/beanutils/beanCompanion.scala
+++ macros/src/main/scala/com/yetu/beanutils/beanCompanion.scala
@@ -14,9 +14,8 @@ object beanCompanionMacro {
 
         // An object definition automatically creates its own constructor, so get a list of all the non-constructor
         // declarations inside the object
-        val nonConstructors = body.flatMap {
-          case DefDef(_, nme.CONSTRUCTOR, _, _, _, _) => None
-          case element => Some(element)
+        val nonConstructors = body.collect {
+          case method @ DefDef(_, name, _, _, _, _) if name != nme.CONSTRUCTOR => method
         }
 
         // Some pattern matching magic to get at the Type instance of the target class
@@ -25,40 +24,33 @@ object beanCompanionMacro {
           case Apply(Select(New(AppliedTypeTree(Ident(_), List(typ))), nme.CONSTRUCTOR), List()) ⇒ typ
         }
         // Of course 7 is not an instance of the Target class... but all we want is to extract the Type of the class
-        val tpe: Type = c.typeCheck(q"(7.asInstanceOf[$targetClass])").tpe
+        val targetType: Type = c.typeCheck(q"(7.asInstanceOf[$targetClass])").tpe
 
         // Get the constructor with the longest parameter list. This will be used for apply and unapply
-        val constructor = tpe.declarations
+        val constructor = targetType.declarations
           .filter(_.name == nme.CONSTRUCTOR)
           .map(_.asMethod)
-          .reduceLeft { (longest, current) ⇒
-            val numParams = current.paramss.head.size
-            val longestParams = longest.paramss.head.size
-
-            if (numParams > longestParams) current else longest
-          }
+          .maxBy(_.paramss.head.size)
 
         // Generate the apply method
         val params = constructor.paramss.head
         val applyParams = params map { p ⇒ q"${p.name.toTermName}: ${p.typeSignature}" }
         val applyBody = params map { p ⇒ q"${p.name.toTermName}" }
-        val applyMethod = q"def apply(..$applyParams) = new $tpe(..$applyBody)"
+        val applyMethod = q"def apply(..$applyParams) = new $targetType(..$applyBody)"
 
         // We need all the accessors to generate the correct calls in unapply due to Java using getX for property x
         // (i.e., upper/lowercase and adding 'get'). We could use just String manipulation but this is safer
-        val accessors = tpe.declarations
-          .filter { decl ⇒
-            val name = decl.name.decoded
-            name.startsWith("get") || name.startsWith("is") || name.startsWith("has")
-          }
-          .map(_.asMethod)
+        val accessors = targetType.declarations.filter { decl ⇒
+          val name = decl.name.decoded
+          name.startsWith("get") || name.startsWith("is") || name.startsWith("has")
+        }.map(_.asMethod)
 
         // And now generate the unapply method
-        val unapplyTypes = params map { p => q"${p.typeSignature}" }
+        val unapplyTypes = params map { p ⇒ q"${p.typeSignature}" }
         val unapplyBody = params flatMap { p ⇒
-          accessors.find{ _.name.decoded.toLowerCase contains p.name.decoded.toLowerCase}.map(a ⇒ q"obj.$a()")
+          accessors.find { _.name.decoded.toLowerCase contains p.name.decoded.toLowerCase }.map(a ⇒ q"obj.$a()")
         }
-        val unapplyMethod = q"def unapply(obj: $tpe) = Option((..$unapplyBody))"
+        val unapplyMethod = q"def unapply(obj: $targetType) = Option((..$unapplyBody))"
 
         val objectBody = nonConstructors :+ applyMethod :+ unapplyMethod
 
@@ -66,7 +58,7 @@ object beanCompanionMacro {
 object $objectName {
   ..$objectBody
 }"""
-        c.Expr[Any](ret)
+        c.Expr[ Any ](ret)
       }
 
     annottees.map(_.tree) match {
